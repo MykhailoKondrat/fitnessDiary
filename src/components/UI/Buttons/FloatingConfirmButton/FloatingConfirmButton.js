bd954432a9f3c19e8a64f29b276fbf9e8aeebc87
Mykhailo Kondrat <mykhailo.kondrat@gmail.com>
2020-07-09 17:47:20 +0300

    add logout handler

--- src/components/UI/Buttons/FloatingConfirmButton/FloatingConfirmButton.js
+++ src/components/UI/Buttons/FloatingConfirmButton/FloatingConfirmButton.js
@@ -4,7 +4,6 @@ import classes from "./FloatingConfirmButton.module.scss";
 const FloatingConfirmButton = (props) => {
   return (
     <button
-      key={props.key}
       type={props.type}
       onClick={props.click}
       className={classes.FloatingButton}
--- src/containers/hoc/Layout/Layout.js
+++ src/containers/hoc/Layout/Layout.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
 import classes from "./Layout.module.scss";
 import Navbar from "../../../components/Navigation/Navbar/Navbar";
 import Toolbar from "../../../components/Navigation/Toolbar/Toolbar";
+import { logoutActionCreator } from "../../Auth/authSlice";
 
 const Layout = (props) => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const handleLogOut = () => {
+    dispatch(logoutActionCreator());
+    history.push("/auth");
+  };
   return (
     <div className={classes.Wrapper}>
-      <Toolbar confirmIcon="Logout">
+      <Toolbar confirmIcon="Logout" confirmAction={handleLogOut}>
         Welcome, Mykhailo Kondrat!
         {props.username}{" "}
       </Toolbar>
